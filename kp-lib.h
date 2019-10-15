9f1a6ef12a07ab9455ea464741438d921f2b5e71
kp-void <contact@cody-p.me>
2019-10-14 19:42:18 -0700

    Updated lib

--- kp-lib.h
+++ kp-lib.h
@@ -3,8 +3,10 @@
 * I store this file in source control and link it from multiple projects
 *********************************************************************************/
 
-// COMMENT TEMPLATES
-// PROJECT HEADER
+// ==========================================================================
+// PA COMMENT TEMPLATE
+// ==========================================================================
+
 /*
 * Programmer: 
 * Class: CptS 121, Lab Section 17L
@@ -13,7 +15,9 @@
 * Description:
 */
 
-// FUNCTION TEMPLATE
+// ==========================================================================
+// FUNCTION COMMENT TEMPLATE
+// ==========================================================================
 /*
 Input parameters:
 Output parameters:
@@ -25,64 +29,67 @@ Description:
 Date Created:
 */
 
+
 #ifndef KP_LIB_H
 #define KP_LIB_H
 
-// my defines
-#define SUCCESS 0
+	// ==========================================================================
+	// CONSTANTS
+	// ==========================================================================
+	#define SUCCESS 0
 
-// platform-dependent code ==========================================================================
+	// ==========================================================================
+	// OS DEPENDENT
+	// ==========================================================================
+	#if defined(_WIN32) || defined (_WIN64) // visual studio 2019
+		#define CLEAR_SCREEN system("cls")
+	#elif defined (__linux__)
+		// TODO: change this to something more platform independent.
+		#define CLEAR_SCREEN printf("\033[H\033[J")
+	#elif defined (OS_OVERRIDE)
 
-#if defined(_WIN32) or defined (_WIN64) // visual studio 2019
+	#else
+		#error Unrecognized OS. Please find out how to define for this system.
+	#endif
 
-#define CLEAR_SCREEN system("cls")
-#define PAUSE printf("Press enter to continue."); WAIT_FOR_ENTER
+	// ==========================================================================
+	// COMPILER DEPENDENT
+	// ==========================================================================
+	#if defined (_MSC_VER)
+	// visual studio
+		#define FORCE_INLINE
 
-#elif defined (__linux__)
+	#elif defined(__GNUC__)
+		// gcc
+		#define FORCE_INLINE __attribute__((always_inline))
 
-#error Linux macros not yet defined
+	#endif
 
-#else
+	// ==========================================================================
+	// GLOBAL MACROS
+	// ==========================================================================
+	#define STATIC_ASSERT(e) (sizeof(struct { int:-!(e); })) // shamelessly stolen from the linux kernel
 
-#error unrecognized OS
+	#define DUMP_SCAN for (int c; (c = getchar()) != '\n' && c != EOF;) {}
 
-#endif
+	#define WAIT_FOR_ENTER for (int c; (c =getchar()) != '\n' && c != EOF;);
 
-#if defined (_MSC_VER)
-// visual studio
-#define FORCE_INLINE
-#elif defined(__GNUC__)
-// gcc
-#define FORCE_INLINE __attribute__((always_inline))
-#endif
+	#define PAUSE printf("Press enter to continue."); WAIT_FOR_ENTER
 
-// Debug Stuff ======================================================================================
-#ifdef DEBUG
-#define ASSERT(e) (sizeof(struct { int:-!(e); })) // shamelessly stolen from the linux kernel
-#else
-#define ASSERT(e)
-#endif
+	#define UNIMPLEMENTED(S) CLEAR_SCREEN; printf_s(#S " is unimplemented.\n"); PAUSE
 
-// Macros ===========================================================================================
-#define DUMP_SCAN for (int c; (c = getchar()) != '\n' && c != EOF;) {}
-#define UNIMPLEMENTED(S) CLEAR_SCREEN; printf_s(#S " is unimplemented.\n"); PAUSE
+	#define LOOPING_INCREMENT(token, max_value) \
+	if (token == max_value) { \
+		token = 0; \
+	} \
+	else ++(token)
 
-// C++ EXCLUSIVE
-#ifdef __cplusplus
+	#define RANDOM_RANGE(start, end) ASSERT(end > start) ((rand() % end)+start) // this is inclusive
 
-#endif
+	#define STRINGIFY(n) #n
 
-// MACROS
-#define LOOPING_INCREMENT(token, max_value) \
-if (token == max_value) { \
-	token = 0; \
-} \
-else ++(token)
+	#define COUNT_DIGITS(n) (sizeof(STRINGIFY(n)) -1); { int _verify = n;}
 
-// inclusive
-#define RANDOM_RANGE(start, end) ASSERT(end > start) ((rand() % end)+start)
+#endif KP_LIB_H
 
-#define STRINGIFY(n) #n
-#define COUNT_DIGITS(n) (sizeof(STRINGIFY(n)) -1)
-#define WAIT_FOR_ENTER fflush(stdin); for (int c; (c =getchar()) != '\n' && c != EOF;);
-#endif
+
